efd08a3f013578e12dee5efe90d77419e79b8622
Kyrie11 <770790858@qq.com>
2023-02-13 19:36:00 +0800

    修改pdr估计网络

--- ysz/pdr_lstm.py
+++ ysz/pdr_lstm.py
@@ -1,6 +1,6 @@
 #输入 加速度、角速度
 #输出 移动步长
-
+#参考论文 https://ceur-ws.org/Vol-2498/short60.pdf
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -30,49 +30,89 @@ class CNN_LSTM(nn.Module):
         Ks = args.kernel_sizes
         self.C = C
         self.embed = nn.Embedding(V, D, padding_idx=args.paddingId)
+        if(args.word_Embedding):
+            self.embed.weight.data.copy_(args.pretrained_weight)
 
-        self.convs1 = [nn.Conv2d(in_channels=Ci,
-                                out_channels=Co,
-                                kernel_size=K,
-                                stride=D
-                                ) for K in Ks]
-        self.dropout = nn.Dropout(args.dropout)
+        self.convs = nn.Sequential(
+            nn.Conv2d(in_channels=1,
+                      out_channels=16,
+                      kernel_size=12,
+                      stride=1,
+                      padding=0),
+            nn.Dropout(p=0.5),
+            nn.ReLU()
+        )
 
+        self.conv1_2 = nn.Sequential(
+            nn.Conv2d(in_channels=16,
+                      out_channels=16,
+                      kernel_size=12,
+                      stride=1,
+                      padding=0),
+            nn.Dropout(p=0.5),
+            nn.ReLU()
+        )
+
+        self.conv1_3 = nn.Sequential(
+            nn.Conv2d(in_channels=16,
+                      out_channels=16,
+                      kernel_size=12,
+                      stride=1,
+                      padding=0),
+            nn.Dropout(p=0.5),
+            nn.ReLU()
+        )
+
+        self.conv2_2 = nn.Sequential(
+            nn.Conv2d(in_channels=1,
+                      out_channels=16,
+                      kernel_size=24,
+                      stride=1,
+                      padding=0),
+            nn.Dropout(p=0.5),
+            nn.ReLU()
+        )
+
+        self.conv2_3 = nn.Sequential(
+            nn.Conv2d(in_channels=16,
+                      out_channels=16,
+                      kernel_size=24,
+                      stride=1,
+                      padding=0),
+            nn.Dropout(p=0.5),
+            nn.ReLU()
+        )
         if(self.args.cuda is True):
             for conv in self.convs1:
                 conv = conv.cuda()
 
 
-        self.lstm = lstm = nn.LSTM(D, self.hidden_dim, dropout=args.dropout, num_layers=self.num_layers)
+        self.lstm = nn.LSTM(16, 64, dropout=args.dropout, num_layers=1)
 
         L = len(Ks) * Co + self.hidden_dim
         self.hidden2label1 = nn.Linear(L, L // 2)
         self.hidden2label2 = nn.Linear(L // 2, C)
 
+        self.fc1 = nn.Sequential(
+            nn.Linear(in_features=64,
+                      out_features=128),
+            nn.BatchNorm1d(),
+            nn.ReLU()
+        )
+
+        self.fc2 = nn.Sequential(
+            nn.Linear(in_features=128,
+                      out_features=1),
+            nn.ReLU()
+        )
+
     def forward(self, x):
-        embed = self.embed(x)
-
-        cnn_x = embed
-        cnn_x = torch.transpose(cnn_x, 0, 1)
-        cnn_x = cnn_x.unsqueeze(1)
-        cnn_x = [F.relu(conv(cnn_x)).squeeze(3) for conv in self.convs1]
-        cnn_x = [F.max_pool1d(i, i.size(2)).squeeze(2) for i in cnn_x]
-        cnn_x = torch.cat(cnn_x, 1)
-        cnn_x = self.dropout(cnn_x)
-
-        lstm_x = embed.view(len(x), embed.size(1), -1)
-        lstm_out, _ = self.lstm(lstm_x)
-        lstm_out = torch.transpose(lstm_out, 0, 1)
-        lstm_out = torch.transpose(lstm_out, 1, 2)
-        lstm_out = F.max_pool1d(lstm_out, lstm_out.size(2)).squeeze(2)
-
-        cnn_x = torch.transpose(cnn_x, 0, 1)
-        lstm_out = torch.transpose(lstm_out, 0, 1)
-        cnn_lstm_out = torch.cat((cnn_x, lstm_out), 0)
-        cnn_lstm_out = torch.transpose(cnn_lstm_out, 0, 1)
-
-        cnn_lstm_out = self.hidden2label1(F.tanh(cnn_lstm_out))
-        cnn_lstm_out = self.hidden2label2(F.tanh(cnn_lstm_out))
-
-        logit = cnn_lstm_out
-        return logit
+        x = self.convs(x)
+        x1 = self.conv1_2(x)
+        x1 = self.conv1_3(x1)
+        x2 = self.conv2_2(x)
+        x2 = self.conv2_3(x2)
+        x = torch.cat([x1, x2], 0)
+        x = self.lstm(x)
+        x = self.fc1(x)
+        x = self.fc2(x)
